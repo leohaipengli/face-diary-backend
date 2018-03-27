f6844fec37464b87e658680e367cfc8b44d96da7
haipengl <haipengl@sfu.ca>
2018-03-26 19:55:19 -0700

    set origin as localhost 3000

--- app.js
+++ app.js
@@ -56,12 +56,13 @@ app.use(serveStatic(path.join(__dirname, 'public')));
 app.use(serveStatic(path.join(__dirname, 'media')));
 
 var corsOptions = {
+  origin:['http://localhost:3000'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   methods:['GET','POST', 'OPTIONS'],
   credentials: true // enable set cookie
-}
+};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
