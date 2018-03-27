56f7a4a8eb27262119d8a700b39a1b6d4aabab19
haipengl <haipengl@sfu.ca>
2018-03-26 17:05:47 -0700

    deal with OPTIONS request

--- app.js
+++ app.js
@@ -54,6 +54,22 @@ app.use(serveStatic(path.join(__dirname, 'public')));
 // serve media files
 app.use(serveStatic(path.join(__dirname, 'media')));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+    //move on
+    next();
+  }
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/emotion', emotion);
