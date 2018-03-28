2f887092dbe90b45ceac2c9779606e56b4369d53
haipengl <haipengl@sfu.ca>
2018-03-27 20:36:48 -0700

    set CORS headers

--- app.js
+++ app.js
@@ -58,11 +58,30 @@ app.use(serveStatic(path.join(__dirname, 'media')));
 var corsOptions = {
   origin:['http://localhost:3000'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  methods:['GET','POST', 'OPTIONS'],
+  methods:['GET', 'POST', 'OPTIONS'],
   credentials: true // enable set cookie
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
