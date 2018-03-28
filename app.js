f246ae81d3fcd5880ec6703c25ce0542b00795ed
haipengl <haipengl@sfu.ca>
2018-03-27 21:48:28 -0700

    send 200 if OPTIONS

--- app.js
+++ app.js
@@ -79,6 +79,11 @@ app.use(function (req, res, next) {
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+
   // Pass to next layer of middleware
   next();
 });
