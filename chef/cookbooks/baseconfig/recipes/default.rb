9fa471a5dbc66b7aec7723bf34c52a18b98074ec
haipengl <haipengl@sfu.ca>
2018-03-28 23:35:42 -0700

    config chef to install node, install forever, config nginx server

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -13,12 +13,33 @@ package "nginx"
 package "tree"
 package "ack-grep"
 package "postgresql"
+
+execute 'install_node_1' do
+  command 'curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -'
+end
+
+execute 'install_node_2' do
+  command 'apt install -y nodejs build-essential'
+end
+
+# install forever
+execute 'install_forever' do
+  command 'npm install forever -g'
+end
+
+# use forever to run project
+execute 'run_server' do
+  command 'PORT=3001 forever start bin/www'
+  cwd '/home/haipengl/project/'
+end
+
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
 end
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
+
 # Create index.html to nginx root
 cookbook_file '/var/www/html/index.html' do
   source 'index.html'
@@ -26,3 +47,18 @@ cookbook_file '/var/www/html/index.html' do
   group 'root'
   mode '0755'
 end
+
+# move facediary server config file
+cookbook_file 'facediary' do
+  path "/etc/nginx/sites-available"
+end
+# link to site-enabled
+execute 'link_to_site_enabled' do
+  command 'ln -s /etc/nginx/sites-available/facebook-login /etc/nginx/sites-enabled'
+end
+
+# restart nginx
+service "nginx" do
+  action :restart
+end
+
